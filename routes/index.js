463711d2399835e8829683660d169dbd3470f9aa
eneax <19818937+eneax@users.noreply.github.com>
2018-07-04 21:42:43 +0200

    Exploring Routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = {name: 'Wes', age: 100, cool: true};
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  // res.send(req.query.name);
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
+
+
+/*
+req --> it's an obj full of info that is coming in
+
+res --> it's an obj full of methods that is sending data back to the user
+
+next --> it's a middleware (it passes data to somebody else to handle)
+*/ 
