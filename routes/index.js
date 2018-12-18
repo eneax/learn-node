5977fa97f4951bc677ce40bfaa85c141831a92cd
eneax <19818937+eneax@users.noreply.github.com>
2018-12-18 21:45:41 +0100

    Intro to routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  res.send(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join(' ');
+  res.send(reverse);
 });
 
 module.exports = router;
+
+
+
+/* 
+* req -> contains all the data
+* res -> sends data back to the user
+*/
