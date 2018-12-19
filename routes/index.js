487ddd2be01541bab7b8563a987858c0791c745e
eneax <19818937+eneax@users.noreply.github.com>
2018-12-19 10:30:12 +0100

    Intro to controllers and MVC pattern

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
-  // res.json(enea);
-  // req.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'enea',
-    dog: req.query.dog,
-    title: 'I love food'
-  });
-});
+router.get('/', storeController.homePage);
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+* req -> contains all the data
+* res -> sends data back to the user
+
+
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join(' ');
   res.send(reverse);
 });
 
-module.exports = router;
-
-
-
-/* 
-* req -> contains all the data
-* res -> sends data back to the user
 */
