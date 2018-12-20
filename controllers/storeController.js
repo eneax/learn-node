7681ae832285b6c8bae85a500056919cce4b3061
eneax <19818937+eneax@users.noreply.github.com>
2018-12-20 20:40:54 +0100

    Remove comments for quering the db

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -17,9 +17,8 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-  // 1. Query the database for a list of all stores
+  // Query the database for a list of all stores
   const stores = await Store.find();
-  console.log(stores);
   res.render('stores', { title: 'Stores', stores: stores });
 };
 
