0816fda240ff42a079c36aef2139acd269729bdf
eneax <19818937+eneax@users.noreply.github.com>
2018-12-22 18:31:21 +0100

    Upload and Resize images with middleware

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
 
 // Check if the user uploads a picture
 const multerOptions = {
@@ -25,8 +27,25 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-// uploads file to memory
+// Upload file to memory
 exports.upload = multer(multerOptions).single('photo');
+
+// Resize image before uploading
+exports.resize = async (req, res, next) => {
+  // Check if there is no file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written our photo to the filesystem, keep going!
+  next();
+}
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
