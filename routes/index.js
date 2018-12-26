cf7f6d56f41c46545a8e334ab74c0b835c2e64a5
eneax <19818937+eneax@users.noreply.github.com>
2018-12-26 11:02:23 +0100

    Create validateRegister method

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,11 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login/', userController.loginForm);
 router.get('/register/', userController.registerForm);
 
+// 1. Validate the registration data
+// 2. Register the user
+// 3. Log them in
+router.post('/register', userController.validateRegister);
+
 module.exports = router;
 
 
