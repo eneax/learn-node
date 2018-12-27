84f25e2de20f2730f72f87b3e67d13bfab5c21ad
eneax <19818937+eneax@users.noreply.github.com>
2018-12-27 11:36:31 +0100

    Login user after registration

---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+const passport = require('passport'); // needed in order to log everybody in
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed Login!',
+  successRedirect: '/',
+  successFlash: 'You are now logged in!'
+});
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -33,7 +34,8 @@ router.get('/register/', userController.registerForm);
 // 3. Log them in
 router.post('/register', 
   userController.validateRegister,
-  userController.register  
+  userController.register,
+  authController.login
 );
 
 module.exports = router;
