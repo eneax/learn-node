c423eeabf7afb75715dfe2a4650a9d17f29e9e2b
eneax <19818937+eneax@users.noreply.github.com>
2018-12-28 09:43:26 +0100

    Add isLoggedIn middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out!');
   res.redirect('/');
 };
+
+exports.isLoggedIn = (req, res, next) => {
+  // Check if the user is authenticated
+  if (req.isAuthenticated()) {
+    next(); // Carry on! They are logged in!
+    return;
+  }
+  req.flash('error', 'Oops! You must be logged in!');
+  res.redirect('/login');
+};
