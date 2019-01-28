82b6c10bc3043ddf5eb364ef7254c256d5de15a2
eneax <19818937+eneax@users.noreply.github.com>
2019-01-28 11:22:37 +0100

    Add Reviews Data Model

--- start.js
+++ start.js
@@ -22,9 +22,11 @@ mongoose.connection.on('error', (err) => {
 // Import all of our models
 require('./models/Store');
 require('./models/User');
+require('./models/Review');
 
 // Start our app!
 const app = require('./app');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
