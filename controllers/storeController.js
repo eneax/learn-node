aa0ec916eb41ce4e3b6739b75be28c8fe9b13b7a
eneax <19818937+eneax@users.noreply.github.com>
2019-01-28 16:56:46 +0100

    Advanced Aggregation

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -167,6 +167,11 @@ exports.getHearts = async (req, res) => {
   res.render('stores', { title: 'Hearted Stores', stores: stores });
 };
 
+exports.getTopStores = async (req, res) => {
+  const stores = await Store.getTopStores();
+  res.render('topStores', { stores, title: '⭐ Top Stores!' });
+};
+
 
 /*
 
--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,10 @@ router.get('/map', storeController.mapPage);
 
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
-router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));;
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
+
+router.get('/top', catchErrors(storeController.getTopStores));
+
 
 // API
 router.get('/api/search', catchErrors(storeController.searchStore));
